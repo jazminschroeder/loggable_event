9a9d2694a7272686846049081c25ae4132774225
Jazmin Schroeder <jazminschroeder@gmail.com>
2012-07-21 14:43:24 -0500

    Add description

--- lib/generators/loggable_event/install/loggable_event_generator.rb
+++ lib/generators/loggable_event/install/loggable_event_generator.rb
@@ -4,6 +4,7 @@ class LoggableEventGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   
   def generate_model
+    say "copying log.rb"
     copy_file 'log.rb', 'app/models/log.rb'
     migration_template 'create_logs.rb', 'db/migrate/create_logs.rb'
   end
