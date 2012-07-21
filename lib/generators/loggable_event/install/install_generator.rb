6c7c3eee012c328c93f699de43e75c02b86d0d71
Jazmin Schroeder <jazminschroeder@gmail.com>
2012-07-21 18:18:48 -0500

    Add generator install

--- lib/generators/loggable_event/install/install_generator.rb
+++ lib/generators/loggable_event/install/install_generator.rb
@@ -1,16 +1,20 @@
-require 'rails/generators/migration'
+require 'rails/generators/active_record'
 module LoggableEvent
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
+     # include Rails::Generators::Migration
       source_root File.expand_path('../templates', __FILE__)
   
       def generate_model
-        say "copying log.rb"
         copy_file 'log.rb', 'app/models/log.rb'
         migration_template 'create_logs.rb', 'db/migrate/create_logs.rb'
+        say "Start logging!"
       end
-  
+      
+      def self.next_migration_number
+        ActiveRecord::Generators::Base.next_migration_number
+      end
+      
     end
   end
 end  
