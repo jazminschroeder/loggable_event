5ee95da933da95acd837dc030e89a05da26471fb
Jazmin Schroeder <jazminschroeder@gmail.com>
2012-07-21 22:23:03 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# LoggableEvent #
+gem 'loggable_event'
+
+rails generate loggable_event:install
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ ActiveRecord::Base.establish_connection(:adapter => 'sqlite3',
                                         :database => File.dirname(__FILE__) + '/support/loggable_event.sqlite3')
 load File.dirname(__FILE__) + '/support/schema.rb'
 load File.dirname(__FILE__) + '/support/models.rb'                                  
-load File.dirname(__FILE__) + '/../lib/generators/templates/log.rb'
+load File.dirname(__FILE__) + '/../lib/generators/loggable_event/install/templates/log.rb'
 
 DatabaseCleaner.strategy = :truncation
 
